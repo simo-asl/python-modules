07bbb1d553efedce1d4aef5bdda20230fbe27bf9
Mohammed El-Aslaouy <mel-asla@e1r5p9.1337.ma>
2025-12-15 18:14:39 +0100

    .

---
+++ python_module_01/ex0/ft_garden_intro.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    plant = "Rose"
+    height = 25
+    age = 30
+    print(f"Plant: {plant}\nHeight: {height}cm\nAge: {age} days\n")
+    print("=== End of Program ===")
