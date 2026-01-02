1746153dc837b67520592b4530d8d47962e5a870
Mohammed El-Aslaouy <mel-asla@e1r2p4.1337.ma>
2026-01-02 02:46:52 +0100

    .

---
+++ python_module_00/ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,7 @@
+def ft_count_harvest_iterative():
+    days = int(input("Days until harvest: "))
+    loop = 1
+    while loop <= days:
+        print("Day", loop)
+        loop += 1
+    print("Harvest time!")
