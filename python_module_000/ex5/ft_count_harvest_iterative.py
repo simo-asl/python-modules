55f8b7287c4903d30b5af1cad4907f8a67845e73
Mohammed El-Aslaouy <mel-asla@e1r2p4.1337.ma>
2026-01-02 02:47:37 +0100

    Removing wrog folder

--- python_module_000/ex5/ft_count_harvest_iterative.py
+++
@@ -1,7 +0,0 @@
-def ft_count_harvest_iterative():
-    days = int(input("Days until harvest: "))
-    loop = 1
-    while loop <= days:
-        print("Day", loop)
-        loop += 1
-    print("Harvest time!")
--- python_module_000/ex6/ft_garden_summary.py
+++
@@ -1,6 +0,0 @@
-def ft_garden_summary():
-    name = input("Enter garden name: ")
-    plants = int(input("Enter number of plants: "))
-    print("Garden:", name)
-    print("Plants:", plants)
-    print("Status: growing well!")
