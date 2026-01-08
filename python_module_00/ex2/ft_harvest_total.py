ad60cd08ba4bf1475d1215970c4e3ca422b1cdcd
Mohammed El-Aslaouy <mel-asla@e1r2p4.1337.ma>
2026-01-08 03:27:05 +0100

    .

--- python_module_00/ex2/ft_harvest_total.py
+++ python_module_00/ex2/ft_harvest_total.py
@@ -2,4 +2,5 @@ def ft_harvest_total():
     day1 = int(input("Day 1 harvest: "))
     day2 = int(input("Day 2 harvest: "))
     day3 = int(input("Day 3 harvest: "))
-    print("Total harvest: ", day1 + day2 + day3)
+    sum = day1 + day2 + day3
+    print("Total harvest:", sum)
