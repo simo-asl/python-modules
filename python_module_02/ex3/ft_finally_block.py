d1f7151efe8e662a5cf6a3b4d662732786609f80
Mohammed El-Aslaouy <mel-asla@e1r11p3.1337.ma>
2026-01-23 16:29:36 +0100

    Fixig output

--- python_module_02/ex3/ft_finally_block.py
+++ python_module_02/ex3/ft_finally_block.py
@@ -1,27 +1,30 @@
-def water_plants(plant_list: list[str | None]) -> None:
+def water_plants(plant_list: list, plant: list) -> None:
     print("Opening watering system")
     try:
-        for plant in plant_list:
-            if plant is None:
+        for data in plant:
+            if data not in plant_list: 
                 print("Error: Cannot water None - invalid plant!")
                 return
-            print("Watering " + plant)
+            print(f"Watering {data}")
     except TypeError:
         print("Error: Invalid plant list!")
     finally:
         print("Closing watering system (cleanup)")
+    print("Watering completed successfully!\n")
 
 
 def test_watering_system() -> None:
-    print("=== Garden Watering System ===")
+    print("=== Garden Watering System ===\n")
 
     print("Testing normal watering...")
-    water_plants(["tomato", "lettuce"])
-    print("Watering completed successfully!")
+    plant_list: list = ["tomato", "lettuce", "carrots"]
+    water_plants(plant_list, plant_list)
 
     print("Testing with error...")
-    water_plants(["tomato", None])
-    print("Cleanup always happens, even with errors!")
+    search_list: list = ["tomato", "khyar"]
+
+    water_plants(plant_list, search_list)
+    print("\nCleanup always happens, even with errors!")
 
 
 if __name__ == "__main__":
