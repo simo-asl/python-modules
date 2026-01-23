27bc94aa973ccc9b5fb3cae51e82c71b09b70bcd
Mohammed El-Aslaouy <mel-asla@e1r11p3.1337.ma>
2026-01-23 15:49:11 +0100

    Raising

--- python_module_02/ex0/ft_first_exception.py
+++ python_module_02/ex0/ft_first_exception.py
@@ -1,33 +1,39 @@
 def check_temperature(temp_str: str) -> int | None:
-    try:
-        temp = int(temp_str)
+    temp = int(temp_str)
 
-        if temp < 0:
-            print(f"Error: {temp}°C is too cold for plants (min 0°C)")
-        elif temp > 40:
-            print(f"Error: {temp}°C is too hot for plants (max 40°C)")
-        else:
-            print(f"Temperature {temp}°C is perfect for plants!")
-            return temp
-
-    except ValueError:
-        print(f"Error: '{temp_str}' is not a valid number")
+    if temp < 0:
+        raise ValueError(f"Error: {temp}°C is too cold for plants (min 0°C)")
+    elif temp > 40:
+        raise ValueError(f"Error: {temp}°C is too hot for plants (max 40°C)")
+    else:
+        print(f"Temperature {temp}°C is perfect for plants!")
 
 
 def test_temperature_input() -> None:
     print("=== Garden Temperature Checker ===\n")
+    try:
+        print("Testing temperature: 25")
+        check_temperature("25")
+    except ValueError as error:
+        print(error)
 
-    print("Testing temperature: 25")
-    check_temperature("25")
+    try:
+        print("\nTesting temperature: abc")
+        check_temperature("abc")
+    except ValueError as error:
+        print(error)
 
-    print("\nTesting temperature: abc")
-    check_temperature("abc")
+    try:
+        print("\nTesting temperature: 100")
+        check_temperature("100")
+    except ValueError as error:
+        print(error)
 
-    print("\nTesting temperature: 100")
-    check_temperature("100")
-
-    print("\nTesting temperature: -50")
-    check_temperature("-50")
+    try:
+        print("\nTesting temperature: -50")
+        check_temperature("-50")
+    except ValueError as error:
+        print(error)
 
     print("\nAll tests completed - program didn't crash!")
 
