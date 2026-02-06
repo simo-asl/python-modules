9a0443591e9503e322d81ff573750fb8c1985bdc
Mohammed El-Aslaouy <mel-asla@e1r11p4.1337.ma>
2026-02-06 10:08:00 +0100

    Removing exit func

--- python_module_03/ex1/ft_score_analytics.py
+++ python_module_03/ex1/ft_score_analytics.py
@@ -1,30 +1,42 @@
 import sys
 
-if __name__ == "__main__":
+
+def machi_main() -> None:
     args = sys.argv[1:]
     print("=== Player Score Analytics ===")
 
-    if args:
+    if not args:
+        print("No scores provided. Usage:"
+              " python3 ft_score_analytics.py <score1> <score2> ...")
+        return
+
+    scores: list[int] = []
+    i = 0
+    while i < len(args):
         try:
-            scores = [int(x) for x in args]
+            n = int(args[i])
         except ValueError:
             print("Error: All scores must be valid integers.")
             print("Usage: python3 ft_score_analytics.py <score1> <score2> ...")
-            sys.exit(1)
-        print(f"Scores processed: {scores}")
-        length = len(scores)
-        print(f"Total players: {length}")
-        total_sum = sum(scores)
-        print(f"Total score: {total_sum}")
-        average = total_sum / length
-        print(f"Average score: {average:.1f}")
-        maximum = max(scores)
-        print(f"High score: {maximum}")
-        minimum = min(scores)
-        print(f"Low score: {minimum}")
-        score_range = maximum - minimum
-        print(f"Score range: {score_range}")
-    else:
-        print(
-            "No scores provided. Usage: python3 ft_score_analytics.py "
-            "<score1> <score2> ...")
+            return
+        scores.append(n)
+        i += 1
+
+    print(f"Scores processed: {scores}")
+    length = len(scores)
+    print(f"Total players: {length}")
+
+    total_sum = sum(scores)
+    print(f"Total score: {total_sum}")
+    average = total_sum / length
+    print(f"Average score: {average:.1f}")
+
+    maximum = max(scores)
+    print(f"High score: {maximum}")
+    minimum = min(scores)
+    print(f"Low score: {minimum}")
+    print(f"Score range: {maximum - minimum}")
+
+
+if __name__ == "__main__":
+    machi_main()
