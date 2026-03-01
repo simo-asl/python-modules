dc42fe440740ef3ec8a497ca443685fc89094735
simo-asl <aslaouisimo1@gmail.com>
2026-03-01 16:56:43 +0000

    At least 2 techniques

--- python_module_06/alchemy/grimoire/spellbook.py
+++ python_module_06/alchemy/grimoire/spellbook.py
@@ -5,3 +5,14 @@ def record_spell(spell_name: str, ingredients: str) -> str:
         return f'Spell recorded: {spell_name} ({validation_result})'
     else:
         return f'Spell rejected: {spell_name} ({validation_result})'
+
+
+def record_spell_injected(
+        spell_name: str,
+        ingredients: str,
+        validator_func
+        ) -> str:
+    validation_result = validator_func(ingredients)
+    if validation_result.endswith(" - VALID"):
+        return f"Spell recorded: {spell_name} ({validation_result})"
+    return f"Spell rejected: {spell_name} ({validation_result})"
