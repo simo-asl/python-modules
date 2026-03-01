f0a0d00cace861f520c6224847cd32ed30264741
simo-asl <aslaouisimo1@gmail.com>
2026-03-01 14:44:04 +0000

    FIXING VALIDATION CONDITION

--- python_module_06/alchemy/grimoire/spellbook.py
+++ python_module_06/alchemy/grimoire/spellbook.py
@@ -1,7 +1,7 @@
 def record_spell(spell_name: str, ingredients: str) -> str:
     import alchemy.grimoire as grimoire
     validation_result = grimoire.validate_ingredients(ingredients)
-    if 'VALID' in validation_result:
+    if validation_result.endswith(" - VALID"):
         return f'Spell recorded: {spell_name} ({validation_result})'
     else:
         return f'Spell rejected: {spell_name} ({validation_result})'
